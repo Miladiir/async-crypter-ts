3de8ee626c280b62b6326fe3d01f69d71880d719
Oliver Conzen <1358063+Miladiir@users.noreply.github.com>
2021-07-19 21:28:36 +0200

    Adds SubtleCrypto (webcrypto) implementation.

---
+++ src/SubtleCrypter.ts
@@ -0,0 +1,148 @@
+import {EncryptionError} from "./errors/EncryptionError";
+import {ErrorMessage} from "./errors/ErrorMessage";
+import {randomBytes} from "crypto";
+import {DecryptionError} from "./errors/DecryptionError";
+import {webcrypto} from "crypto";
+
+const fuck = webcrypto as any;
+const subtle = fuck.subtle as SubtleCrypto;
+/**
+ * Performs AES-256-GCM encryption and decryption of buffers.
+ * The encrypted buffers contain the salt (64 bytes), the iv (16 bytes), the tag (16 bytes)
+ * and finally the encrypted value (rest of the buffer).
+ */
+export class SubtleCrypter {
+
+    /**
+     * The algorithm in use.
+     */
+    private static readonly algorithm = 'AES-GCM';
+
+    /**
+     * The length of an initialization vector in bytes.
+     */
+    private static readonly ivLength = 16;
+
+    /**
+     * The length of a salt in bytes.
+     */
+    private static readonly saltLength = 64;
+
+    /**
+     * The offset of the authentication tag in bytes.
+     */
+    private static readonly encryptedPosition = SubtleCrypter.saltLength + SubtleCrypter.ivLength;
+
+    /**
+     * The secret used to encrypt and decrypt values by this instance.
+     */
+    readonly #secret: Buffer;
+
+    /**
+     * Initiate a new SubtleCrypter instance with the specified secret.
+     * @param secret The secret used to encrypt and decrypt values.
+     * @throws Error when passed anything but a non-empty string or Buffer.
+     */
+    public constructor(secret: string | Buffer) {
+
+        if (secret instanceof Buffer && secret.length !== 0) {
+            this.#secret = secret;
+        } else if (typeof secret === "string" && secret.length !== 0) {
+            this.#secret = Buffer.from(secret);
+        } else {
+            throw new Error(ErrorMessage.INVALID_SECRET);
+        }
+    }
+
+    /**
+     * Encrypt a buffer using AES256-GCM with a new generated salt and a random
+     * initialization vector.
+     * @param value The value to encrypt.
+     * @param additionalAuthenticatedData Additional data that influences encryption outcome.
+     * @returns The uniquely and symmetrically encrypted value.
+     */
+    public async encrypt(value: Buffer, additionalAuthenticatedData?: Buffer): Promise<Buffer | EncryptionError> {
+
+        if (!(value instanceof Buffer)) {
+
+            return new EncryptionError(ErrorMessage.INVALID_ENCRYPTION_VALUE);
+        }
+        const iv = randomBytes(SubtleCrypter.ivLength);
+        const salt = randomBytes(SubtleCrypter.saltLength);
+        const key = await this.generateKey(salt);
+        if (additionalAuthenticatedData != null) {
+            if (additionalAuthenticatedData instanceof Buffer && additionalAuthenticatedData.length !== 0) {
+            } else {
+
+                return new EncryptionError(ErrorMessage.INVALID_AAD);
+            }
+        }
+        let result: Buffer;
+        try {
+            result = Buffer.from(await subtle.encrypt({
+                name: SubtleCrypter.algorithm,
+                iv,
+                additionalData: additionalAuthenticatedData
+            }, key, value));
+        } catch (e) {
+
+            return new EncryptionError("Unknown error");
+        }
+
+        return Buffer.concat([salt, iv, result]);
+    }
+
+    /**
+     * Decrypts a buffer containing the salt, initialization vector, tag and encrypted value.
+     * @param value An encrypted value buffer.
+     * @param additionalAuthenticatedData Additional data that was used to influence encryption outcome.
+     * @returns The decrypted buffer containing the original value.
+     */
+    public async decrypt(value: Buffer, additionalAuthenticatedData?: Buffer): Promise<Buffer | DecryptionError> {
+
+        if (!(value instanceof Buffer) || value.byteLength < SubtleCrypter.encryptedPosition) {
+
+            return new DecryptionError(ErrorMessage.INVALID_DECRYPTION_VALUE);
+        }
+
+        const salt = value.slice(0, SubtleCrypter.saltLength);
+        const key = await this.generateKey(salt);
+        const iv = value.slice(SubtleCrypter.saltLength, SubtleCrypter.encryptedPosition);
+        const encrypted = value.slice(SubtleCrypter.encryptedPosition);
+        if (additionalAuthenticatedData != null) {
+            if (additionalAuthenticatedData instanceof Buffer && additionalAuthenticatedData.length !== 0) {
+            } else {
+
+                return new DecryptionError(ErrorMessage.INVALID_AAD);
+            }
+        }
+        let result: Buffer;
+        try {
+            result = Buffer.from(await subtle.decrypt({
+                name: SubtleCrypter.algorithm,
+                iv,
+                additionalData: additionalAuthenticatedData
+            }, key, encrypted));
+        } catch (e) {
+
+            return new DecryptionError(ErrorMessage.DECRYPTION_FAILED);
+        }
+
+        return result;
+    }
+
+    /**
+     * Generate an encryption key using the provided salt and the secret of the class.
+     * @param salt A salt, either a new random sequence or stored in an encrypted buffer.
+     * @returns An encryption key used to encrypt or decrypt a buffer.
+     */
+    private async generateKey(salt: Buffer): Promise<CryptoKey> {
+        const key = await subtle.importKey("raw", this.#secret, "PBKDF2", false, ["deriveBits", "deriveKey"]);
+        return subtle.deriveKey({
+            name: "PBKDF2",
+            iterations: 100000,
+            hash: "SHA-512",
+            salt
+        }, key, {name: SubtleCrypter.algorithm, length: 256}, false, ["encrypt", "decrypt"]);
+    }
+}
---
+++ test/SubtleCrypter.spec.ts
@@ -0,0 +1,255 @@
+import {expect} from "chai";
+import {SubtleCrypter} from "../src/SubtleCrypter";
+import {DecryptionError, EncryptionError} from "../src";
+
+describe("SubtleCrypter", (): void => {
+
+    const secret = "This is a very secure secret and I will get mad if you think otherwise.";
+    const value = "Try to steal me. I dare you.";
+
+    describe("constructor", (): void => {
+
+        it("should accept non empty string", (): void => {
+
+            expect(new SubtleCrypter(secret)).to.be.instanceOf(SubtleCrypter);
+        });
+
+        it("should not accept empty string", (): void => {
+
+            expect(() => new SubtleCrypter("")).to.throw(Error);
+        });
+
+        it("should accept non empty Buffer", (): void => {
+
+            expect(new SubtleCrypter(Buffer.from(secret))).to.be.instanceOf(SubtleCrypter);
+        });
+
+        it("should not accept empty Buffer", (): void => {
+
+            expect(() => new SubtleCrypter(Buffer.alloc(0))).to.throw(Error);
+        });
+
+        it("should not accept anything else", (): void => {
+
+            // @ts-expect-error Explicitly test incompatible types
+            expect(() => new SubtleCrypter(null)).to.throw(Error);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(() => new SubtleCrypter(undefined)).to.throw(Error);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(() => new SubtleCrypter()).to.throw(Error);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(() => new SubtleCrypter(0)).to.throw(Error);
+        });
+    });
+
+    describe("encrypt", (): void => {
+
+        it("should generate distinct output files (distinct instances)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter1 = new SubtleCrypter(secret);
+            const crypter2 = new SubtleCrypter(secret);
+            const encrypted1 = await crypter1.encrypt(buffer);
+            const encrypted2 = await crypter2.encrypt(buffer);
+            expect(encrypted1.toString()).to.not.equal(encrypted2.toString());
+        });
+
+        it("should generate distinct output files (same instance)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted1 = await crypter.encrypt(buffer);
+            const encrypted2 = await crypter.encrypt(buffer);
+            expect(encrypted1.toString()).to.not.equal(encrypted2.toString());
+        });
+
+        it("should work for an empty Buffer", async (): Promise<void> => {
+
+            const buffer = Buffer.alloc(0);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            expect(encrypted.byteLength).to.be.at.least(64 + 16 + 16);
+        });
+
+        it("should fail for non Buffer values", async (): Promise<void> => {
+
+            const crypter = new SubtleCrypter(secret);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt(123)).to.be.instanceOf(EncryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt("")).to.be.instanceOf(EncryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt(null)).to.be.instanceOf(EncryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt(undefined)).to.be.instanceOf(EncryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt()).to.be.instanceOf(EncryptionError);
+        });
+
+        it("should fail for empty Buffer aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const aad = Buffer.alloc(0);
+            expect(await crypter.encrypt(buffer, aad)).to.be.instanceOf(EncryptionError);
+        });
+
+        it("should fail for any non Buffer type of aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt(buffer, 0)).to.be.instanceOf(EncryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.encrypt(buffer, new Date())).to.be.instanceOf(EncryptionError);
+        });
+    });
+
+    describe("decrypt", (): void => {
+
+        it("should be able to decrypt data (same class instance)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(buffer.toString()).to.equal(decrypted.toString());
+        });
+
+        it("should be able to decrypt data (same password)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter1 = new SubtleCrypter(secret);
+            const encrypted = await crypter1.encrypt(buffer) as Buffer;
+            const crypter2 = new SubtleCrypter(secret);
+            const decrypted = await crypter2.decrypt(encrypted);
+            expect(buffer.toString()).to.equal(decrypted.toString());
+        });
+
+        it("should fail for non Buffer values", async (): Promise<void> => {
+
+            const crypter = new SubtleCrypter(secret);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt(123)).to.be.instanceOf(DecryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt("")).to.be.instanceOf(DecryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt(null)).to.be.instanceOf(DecryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt(undefined)).to.be.instanceOf(DecryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt()).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should be able to decrypt data (same aad)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const additionalData = Buffer.from("Some additional data, like a userId, a random secret or something");
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer, additionalData) as Buffer;
+            const decrypted = await crypter.decrypt(encrypted, additionalData);
+            expect(buffer.toString()).to.equal(decrypted.toString());
+        });
+
+        it("should not be able to decrypt data with wrong password", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter1 = new SubtleCrypter(secret);
+            const encrypted = await crypter1.encrypt(buffer) as Buffer;
+            const wrongSecret = secret.replace("secure", "insecure");
+            const crypter2 = new SubtleCrypter(wrongSecret);
+            const decrypted = await crypter2.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with wrong aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const additionalData = Buffer.from("Some additional data, like a userId, a random secret or something");
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer, additionalData) as Buffer;
+            const decrypted = await crypter.decrypt(encrypted, buffer);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with missing aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const additionalData = Buffer.from("Some additional data, like a userId, a random secret or something");
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer, additionalData) as Buffer;
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with aad when not encrypted using aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const additionalData = Buffer.from("Some additional data, like a userId, a random secret or something");
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            const decrypted = await crypter.decrypt(encrypted, additionalData);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with corrupted data (salt)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            encrypted.writeUInt8(0x00, 12);
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with corrupted data (iv)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            encrypted.writeUInt8(0x00, 64 + 3);
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with corrupted data (tag)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            encrypted.writeUInt8(0x00, 64 + 16 + 2);
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should not be able to decrypt data with corrupted data (data)", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            encrypted.writeUInt8(0x00, 64 + 16 + 16 + 12);
+            const decrypted = await crypter.decrypt(encrypted);
+            expect(decrypted).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should fail for empty Buffer aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const aad = Buffer.alloc(0);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            expect(await crypter.decrypt(encrypted, aad)).to.be.instanceOf(DecryptionError);
+        });
+
+        it("should fail for any non Buffer type of aad", async (): Promise<void> => {
+
+            const buffer = Buffer.from(value);
+            const crypter = new SubtleCrypter(secret);
+            const encrypted = await crypter.encrypt(buffer) as Buffer;
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt(encrypted, 0)).to.be.instanceOf(DecryptionError);
+            // @ts-expect-error Explicitly test incompatible types
+            expect(await crypter.decrypt(encrypted, new Date())).to.be.instanceOf(DecryptionError);
+        })
+    });
+});
